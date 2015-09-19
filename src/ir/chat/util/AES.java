e97c011b25eae24723c3ff6e0ea0b9b7cf64df05
hamed-ehtesham <hamed.ehtesham.university@gmail.com>
2015-09-19 23:58:04 +0430

    Add encryption utils

--- src/ir/chat/util/AES.java
+++ src/ir/chat/util/AES.java
@@ -10,19 +10,19 @@ import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
 
 //import org.apache.commons.codec.binary.Base64;
-/**
- Aes encryption
- */
-public class AES
-{
 
-    private static SecretKeySpec secretKey ;
-    private static byte[] key ;
+/**
+ * Aes encryption
+ */
+public class AES {
+
+    private static SecretKeySpec secretKey;
+    private static byte[] key;
 
     private static String decryptedString;
     private static String encryptedString;
 
-    public static void setKey(String myKey){
+    public static void setKey(String myKey) {
 
 
         MessageDigest sha = null;
@@ -33,7 +33,7 @@ public class AES
             key = sha.digest(key);
             key = Arrays.copyOf(key, 16); // use only first 128 bit
             System.out.println(key.length);
-            System.out.println(new String(key,"UTF-8"));
+            System.out.println(new String(key, "UTF-8"));
             secretKey = new SecretKeySpec(key, "AES");
 
 
@@ -46,25 +46,26 @@ public class AES
         }
 
 
-
     }
 
     public static String getDecryptedString() {
         return decryptedString;
     }
+
     public static void setDecryptedString(String decryptedString) {
         AES.decryptedString = decryptedString;
     }
+
     public static String getEncryptedString() {
         return encryptedString;
     }
+
     public static void setEncryptedString(String encryptedString) {
         AES.encryptedString = encryptedString;
     }
-    public static String encrypt(String strToEncrypt)
-    {
-        try
-        {
+
+    public static String encrypt(String strToEncrypt) {
+        try {
             Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
 
             cipher.init(Cipher.ENCRYPT_MODE, secretKey);
@@ -72,33 +73,28 @@ public class AES
 
             setEncryptedString(Base64.encodeBase64String(cipher.doFinal(strToEncrypt.getBytes("UTF-8"))));
 
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
 
-            System.out.println("Error while encrypting: "+e.toString());
+            System.out.println("Error while encrypting: " + e.toString());
         }
         return null;
     }
-    public static String decrypt(String strToDecrypt)
-    {
-        try
-        {
+
+    public static String decrypt(String strToDecrypt) {
+        try {
             Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5PADDING");
 
             cipher.init(Cipher.DECRYPT_MODE, secretKey);
             setDecryptedString(new String(cipher.doFinal(Base64.decodeBase64(strToDecrypt))));
 
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
 
-            System.out.println("Error while decrypting: "+e.toString());
+            System.out.println("Error while decrypting: " + e.toString());
         }
         return null;
     }
-    public static void main(String args[])
-    {
+
+    public static void main(String args[]) {
         final String strToEncrypt = "My text to encrypt";
         final String strPssword = "encryptor key";
         AES.setKey(strPssword);
@@ -108,7 +104,7 @@ public class AES
         System.out.println("String to Encrypt: " + strToEncrypt);
         System.out.println("Encrypted: " + AES.getEncryptedString());
 
-        final String strToDecrypt =  AES.getEncryptedString();
+        final String strToDecrypt = AES.getEncryptedString();
         AES.decrypt(strToDecrypt.trim());
 
         System.out.println("String To Decrypt : " + strToDecrypt);
